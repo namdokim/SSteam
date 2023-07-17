8cad1187da17216275cf9371039f96267b30b691
756 <756@DESKTOP-84L7IU1>
2023-07-17 16:36:24 +0900

    채팅 테스트 개발관련 이동경로 설정

---
+++ ss/src/main/java/com/ss/demo/controller/CommunityController.java
@@ -0,0 +1,15 @@
+package com.ss.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping (value="/Community")
+@Controller
+public class CommunityController
+{
+	@RequestMapping (value="/ChattingTest.do")
+	public String toChattingTest()
+	{		
+		return "Community/ChattingTest";
+	}
+}
